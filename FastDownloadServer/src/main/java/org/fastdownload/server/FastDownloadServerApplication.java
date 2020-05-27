eb9e1ba8d7e2d0e0b47ba61d5e2a9228dab45f48
ynchu <3319034446@qq.com>
2020-05-27 12:56:22 +0800

    创建服务端项目

---
+++ FastDownloadServer/src/main/java/org/fastdownload/server/FastDownloadServerApplication.java
@@ -0,0 +1,8 @@
+package org.fastdownload.server;
+
+public class FastDownloadServerApplication {
+
+    public static void main(String[] args) {
+        System.out.println("FastDownload yes!");
+    }
+}
