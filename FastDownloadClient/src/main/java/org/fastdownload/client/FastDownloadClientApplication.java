81d43eaa547d94fc4994af82de0d11d65a977273
ynchu <3319034446@qq.com>
2020-05-27 13:05:12 +0800

    创建客户端项目

---
+++ FastDownloadClient/src/main/java/org/fastdownload/client/FastDownloadClientApplication.java
@@ -0,0 +1,8 @@
+package org.fastdownload.client;
+
+public class FastDownloadClientApplication {
+
+    public static void main(String[] args) {
+        System.out.println("FastDownload yes!");
+    }
+}
